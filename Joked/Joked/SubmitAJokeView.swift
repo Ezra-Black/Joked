73c681c11259622df5f086cdb0fb3cbd51ee08cd
Ezra Black <dev.ezrablack@gmail.com>
2020-09-25 11:53:02 -0500

    Depricated Notes

--- Joked/Joked/SubmitAJokeView.swift
+++ Joked/Joked/SubmitAJokeView.swift
@@ -19,16 +19,9 @@ class SubmitAJokeView: UIViewController {
         view.endEditing(true)
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
     @IBAction func submitButtonPressed(_ sender: Any) {
+        #warning("should send network request")
         self.dismiss(animated: true)
     }
     
