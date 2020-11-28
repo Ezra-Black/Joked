865a19d8a305afeeb0127b60009ecbc6ed6e810d
Ezra Black <dev.ezrablack@gmail.com>
2020-11-28 17:32:15 -0600

    fixed FTupleTransaction.o Link with deployment target. deprecated PickerView Added Image Assets

--- Joked/Joked/SubmitAJokeViewController.swift
+++ Joked/Joked/SubmitAJokeViewController.swift
@@ -8,18 +8,18 @@
 
 import UIKit
 
-class SubmitAJokeViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class SubmitAJokeViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var jokeTextField: UITextField!
     @IBOutlet weak var punchlineTextField: UITextField!
     
-    var pickerData: [String] = [String]()
+//    var pickerData: [String] = [String]()
     var controller = NetworkController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        pickerData = ["general", "programming"]
+//        pickerData = ["general"]
         // Do any additional setup after loading the view.
     }
     
@@ -33,31 +33,31 @@ class SubmitAJokeViewController: UIViewController, UIPickerViewDelegate, UIPicke
      }
 
      // Number of columns of data
-     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-         return 1
-     }
+//     func numberOfComponents(in pickerView: UIPickerView) -> Int {
+//         return 1
+//     }
      
      // The number of rows of data
-     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-         return pickerData.count
-     }
+//     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+//         return pickerData.count
+//     }
      
      // The data to return for the row and component (column) that's being passed in
-     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-         return pickerData[row]
-     }
+//     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+//         return pickerData[row]
+//     }
     
     // Capture the picker view selection
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        // This method is triggered whenever the user makes a change to the picker selection.
-        // The parameter named row and component represents what was selected.
-    }
+//    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+//        // This method is triggered whenever the user makes a change to the picker selection.
+//        // The parameter named row and component represents what was selected.
+//    }
 
     @IBAction func submitButtonPressed(_ sender: Any) {
         showAlert()
         #warning("SEND firebase joke to database!")
         controller.createJoke(with: jokeTextField.text ?? "", punchline: punchlineTextField.text ?? "") {
-            print("Success")
+            print("The joke was submitted successfully: \(self.jokeTextField.text), \(self.punchlineTextField.text)")
         }
     }
     
